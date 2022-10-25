a041f85544d0b48513b00cab42c1296f00a92893
강희주 <@LAPTOP-G0D9E46G>
2022-10-25 12:44:57 +0900

    7주차 과제

---
+++ Q1.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Q1 extends JFrame {
+	public Q1() {
+		this.setTitle("Let's study Java");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(400, 200);
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new Q1();
+	}
+}
